56e646fb29302e73698bc5857e41a0f5e80a13f7
joão marcos <joaofrigo1@gmail.com>
2024-10-14 23:47:18 -0300

    Commit contatos

--- projeto_web_pc_residuos/urls.py
+++ projeto_web_pc_residuos/urls.py
@@ -4,6 +4,9 @@ from .views import *
 
 
 urlpatterns = [
-    path('', home_view),
-    path('exemplo/', exemplo_view),
+    path('', home_view, name='home'),  
+    path('exemplo/', exemplo_view, name='exemplo'),  
+    path('contato/', contato_view, name='contate_a_gente'),  
+    path('lista_residuos/', lista_residuos_view, name='lista_de_residuos'),
+    path('adicionar_residuo/', adicionar_residuo_view, name='adicionar_residuo'), 
 ]
--- projeto_web_pc_residuos/views.py
+++ projeto_web_pc_residuos/views.py
@@ -6,7 +6,16 @@ def index(request):
     return HttpResponse("Olá, mundo. Esta é a página inicial do meu aplicativo.")
 
 def home_view(request):
-    return HttpResponse("Essa é a home page")
+    return render(request, 'template_home.html')
 
 def exemplo_view(request):
     return HttpResponse("Essa é a view de exemplo")
+
+def contato_view(request):
+    return render(request, 'contato.html')  
+
+def lista_residuos_view(request):
+    return render(request, 'lista_residuos.html')  
+
+def adicionar_residuo_view(request):
+    return render(request, 'adicionar_residuo.html') 
