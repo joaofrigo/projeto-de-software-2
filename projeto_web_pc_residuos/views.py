88aba185cab0d495abe5e5085361b85ace346f24
AlejandroBrites <104328707+AlejandroBrites@users.noreply.github.com>
2024-11-09 00:05:42 -0300

    Add files via upload

--- projeto_web_pc_residuos/views.py
+++ projeto_web_pc_residuos/views.py
@@ -30,7 +30,34 @@ def lista_residuos_view(request):
     return render(request, 'lista_residuos.html', {'residuos': residuos}) 
 
 def adicionar_residuo_view(request):
-    return render(request, 'adicionar_residuo.html') 
+    if request.method == 'GET':
+        # Aqui você pode capturar os dados passados na URL, exemplo:
+        tipo_resido = request.GET.get('tipoResido')
+        nome = request.GET.get('nome')
+        quantidade = request.GET.get('quantidade')
+        unidade_medida = request.GET.get('unidad_medida')
+        data_geracao = request.GET.get('dataGeracao')
+        localizacao = request.GET.get('localizacao')
+        metodo_disposicao = request.GET.get('metodoDisposicao')
+        estado = request.GET.get('estado')
+        observacoes = request.GET.get('observacoes')
+
+        # Processar os dados ou armazená-los conforme necessário
+        return render(request, 'adicionar_residuo.html', {
+            'tipo_resido': tipo_resido,
+            'nome': nome,
+            'quantidade': quantidade,
+            'unidade_medida': unidade_medida,
+            'data_geracao': data_geracao,
+            'localizacao': localizacao,
+            'metodo_disposicao': metodo_disposicao,
+            'estado': estado,
+            'observacoes': observacoes
+        })
+    
+    
+        
+    
 
 def perfil_residuo_view(request):
     return render(request, 'perfil_residuo.html') 
