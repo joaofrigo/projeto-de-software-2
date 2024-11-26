84c414b5667721e0f41e5ffeb231ed9def75d979
joão marcos <joaofrigo1@gmail.com>
2024-11-26 14:50:46 -0300

    adicionando novo ip

--- projeto_web_pc_residuos/views.py
+++ projeto_web_pc_residuos/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 from datetime import date
 from datetime import datetime
 import mysql.connector
-import googlemaps
+#import googlemaps
 
 
 def index(request):
